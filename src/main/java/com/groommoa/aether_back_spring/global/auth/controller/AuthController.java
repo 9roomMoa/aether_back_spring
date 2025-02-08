27109c7545d4b00005687a5888de0ff7461e1963
EveryPine <starcraft819@gmail.com>
2025-02-08 14:09:13 +0900

    feat: 서버 accessToken 재발급 구현
    - 만료된 accessToken으로 해당 api로 요청이 들어온 경우, 재발급된 accessToken을 성공 응답 객체에 담아 반환
    
    Related to: #6

--- src/main/java/com/groommoa/aether_back_spring/global/auth/controller/AuthController.java
+++ src/main/java/com/groommoa/aether_back_spring/global/auth/controller/AuthController.java
@@ -2,19 +2,23 @@ package com.groommoa.aether_back_spring.global.auth.controller;
 
 import com.groommoa.aether_back_spring.global.auth.service.TokenService;
 import com.groommoa.aether_back_spring.global.common.constants.HttpStatus;
+import com.groommoa.aether_back_spring.global.common.constants.TokenKey;
 import com.groommoa.aether_back_spring.global.common.response.CommonResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 @RestController
 public class AuthController {
@@ -26,7 +30,7 @@ public class AuthController {
      * @param accessToken 서버에서 발급된 JWT access token
      * @return 로그인 성공 응답과 JWT access token
      */
-    @GetMapping("/auth/success")
+    @GetMapping("/success")
     public ResponseEntity<CommonResponse> loginSuccess(@RequestParam String accessToken) {
         Map<String, Object> result = new HashMap<>();
         result.put("accessToken", accessToken);
@@ -42,7 +46,7 @@ public class AuthController {
      * @param userDetails 현재 로그인한 사용자의 정보 (Spring Security 제공)
      * @return 로그아웃 성공 응답
      */
-    @DeleteMapping("/auth/logout")
+    @DeleteMapping("/logout")
     public ResponseEntity<CommonResponse> logout(@AuthenticationPrincipal UserDetails userDetails) {
         // 사용자 계정과 연관된 refresh token 삭제
         tokenService.deleteRefreshToken(userDetails.getUsername());
@@ -51,5 +55,30 @@ public class AuthController {
         CommonResponse response = new CommonResponse(
                 HttpStatus.OK, "로그아웃에 성공했습니다.", null);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/reissue")
+    public ResponseEntity<CommonResponse> reissueAccessToken(HttpServletResponse response) {
+        String newAccessToken = response.getHeader(AUTHORIZATION);
+
+        // 접두사 제거
+        if (StringUtils.hasText(newAccessToken) && newAccessToken.startsWith(TokenKey.TOKEN_PREFIX)) {
+            newAccessToken = newAccessToken.substring(TokenKey.TOKEN_PREFIX.length());
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new CommonResponse(HttpStatus.UNAUTHORIZED, "access token 재발급에 실패했습니다.", null));
+        }
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("accessToken", newAccessToken);
+
+        return ResponseEntity.ok(new CommonResponse(
+                HttpStatus.OK, "access token 재발급에 성공했습니다.", result));
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<CommonResponse> test() {
+        return ResponseEntity.ok(new CommonResponse(
+                HttpStatus.OK, "auth 테스트 요청 입니다.", null));
     }
 }
