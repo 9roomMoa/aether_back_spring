5e1b12ce8ab1fe28a0988efe85d6c01c47beddb2
EveryPine <starcraft819@gmail.com>
2025-02-20 13:42:32 +0900

    build: AetherBackSpringApplication 수정
    - eureka client 구성
    
    Related to: #21

--- src/main/java/com/groommoa/aether_back_spring/AetherBackSpringApplication.java
+++ src/main/java/com/groommoa/aether_back_spring/AetherBackSpringApplication.java
@@ -2,9 +2,6 @@ package com.groommoa.aether_back_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
