336bc3869c6f4ebcaed74c2387f63fa959c81b14
EveryPine <starcraft819@gmail.com>
2025-02-21 23:41:57 +0900

    fix: OAuth2SuccessHandler 수정
    - 인증 성공 리다이렉트 url 생성 방식 변경, 생성된 url은 기존과 동일
    
    Related to: #21

--- src/main/java/com/groommoa/aether_back_spring/global/auth/handler/OAuth2SuccessHandler.java
+++ src/main/java/com/groommoa/aether_back_spring/global/auth/handler/OAuth2SuccessHandler.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -23,7 +22,6 @@ import java.io.IOException;
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
     private final TokenProvider tokenProvider;
-    private static final String URI = "/auth/success";
 
     /**
      * OAuth2 로그인 성공 시 실행되는 메서드
@@ -43,7 +41,8 @@ public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
         tokenProvider.generateRefreshToken(principalDetails, accessToken);
 
         // 클라이언트를 인증 성공 페이지로 리다이렉트
-        String redirectUrl = UriComponentsBuilder.fromUriString(URI)
+        String redirectUrl = UriComponentsBuilder.fromUriString("https://aether.asia")
+                .path("/auth/success")
                 .queryParam("accessToken", accessToken)
                 .build().toUriString();
 
