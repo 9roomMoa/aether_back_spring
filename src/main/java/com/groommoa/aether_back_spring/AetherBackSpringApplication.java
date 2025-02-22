815611da58fd36a610c4eb30cb7b224038d694f3
EveryPine <starcraft819@gmail.com>
2025-02-22 16:26:16 +0900

    fix: 시간대 설정 변경
    - auth-service의 서버 시간대를 UTC +9 (KST)로 변경

--- src/main/java/com/groommoa/aether_back_spring/AetherBackSpringApplication.java
+++ src/main/java/com/groommoa/aether_back_spring/AetherBackSpringApplication.java
@@ -1,12 +1,22 @@
 package com.groommoa.aether_back_spring;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableMongoAuditing
 public class AetherBackSpringApplication {
+
+	private final String timeZone = "Asia/Seoul";
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone(timeZone));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AetherBackSpringApplication.class, args);
